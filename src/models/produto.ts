ee0558584507cd9567bea4f31cf303b0a4eac54d
Guilhermedcdias <guilhermedcdias.2021@gmail.com>
2022-11-17 16:54:50 -0300

    adicionando getters e setters dos produtos e serviços

--- src/models/produto.ts
+++ src/models/produto.ts
@@ -1,3 +1,39 @@
 export default class Produto {
     public nome!: string
+    private marca!: string
+    private valor!: string
+    private codigo!: string
+
+    constructor(nome: string, marca: string, valor:string, codigo:string){
+        this.codigo = codigo;
+        this.nome = nome;
+        this.marca = marca;
+        this.valor = valor;
+    }
+
+
+    public get getnome(): string{
+        return this.nome
+    }
+    public get getcodigo(): string {
+        return this.codigo
+    }
+    public get getvalor(): string{
+        return this.valor
+    }
+    public get getmarca(): string{
+        return this.marca
+    }
+    public setnome(nome: string){
+        this.nome = nome
+    }
+    public setcodigo(codigo: string){
+        this.codigo = codigo
+    }
+    public setmarca(marca: string){
+        this.marca = marca
+    }
+    public setvalor(valor: string){
+        this.valor = valor
+    }
 }
--- src/models/servico.ts
+++ src/models/servico.ts
@@ -1,3 +1,24 @@
 export default class Servico {
     public nome!: string
+    private desc!: string
+    private valor!: string
+
+    public get getnome(): string{
+        return this.nome
+    }
+    public get getdesc(): string{
+        return this.desc
+    }
+    public get getvalor(): string{
+        return this.valor
+    }
+    public setnome(nome: string){
+        this.nome = nome
+    }
+    public setdesc(desc: string){
+        this.desc = desc
+    }
+    public setvalor(valor: string){
+        this.valor = valor
+    }
 }
